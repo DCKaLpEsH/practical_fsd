ffb7474859bc6cc9e2cc24dca19da987f0304a3c
DCKaLpEsH <chaudharikalpesh1700@gmail.com>
2023-01-01 23:41:08 +0530

    initial commit

---
+++ open_a_page/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <h2>Text Field</h2>
+    <form action="/action_page.php">
+        <label for="fname">Favourite City</label>
+        <input type="text" id="fname" name="fname"><br>
+        <input type="submit" value="Submit">    
+    </form>
+</body>
+</html>
